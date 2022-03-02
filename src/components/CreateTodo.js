3e0d0da281d76bf50d7d10319efba8d285d77c11
FluffyKas <maruzs.zso@gmail.com>
2022-03-02 15:32:40 +0100

    Adding button for dark mode. Adding trim to input text in CreateTodo.

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,24 +1,24 @@
 import './CreateTodo.scss';
 
-const CreateTodo = ({setInputText, setTodos, todos, inputText}) => {
+const CreateTodo = ({ inputText, setInputText, todos, setTodos }) => {
   const handleChange = (e) => {
-    setInputText(e.target.value);
+    setInputText(e.target.value.trimStart());
   }
   const submitHandler = (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
       e.preventDefault();
       setTodos([
-        ...todos, {text: inputText, completed: false, id: Math.floor(Math.random()*1000)}
+        ...todos, { text: inputText.trim(), completed: false, id: Math.floor(Math.random() * 1000) }
       ]);
       setInputText('');
     }
   }
 
-  return ( 
+  return (
     <form>
-      <input type="text" onChange={handleChange} onKeyPress={submitHandler} value={inputText} placeholder="Create a new todo..."/>
+      <input type="text" onChange={handleChange} onKeyPress={submitHandler} value={inputText} placeholder="Create a new todo..." />
     </form>
-   );
+  );
 }
- 
+
 export default CreateTodo;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import './Header.scss';
+// import { ReactComponent as LightModeIcon } from '../images/icon-sun.svg';
+import { ReactComponent as DarkModeIcon } from '../images/icon-moon.svg';
 
 const Header = () => {
   return (
     <header>
       <h1>TODO</h1>
-      <button className="theme-toggle">O</button>
+      <button className="theme-toggle">
+        <DarkModeIcon />
+      </button>
     </header>
   );
 }
