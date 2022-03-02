3bb724e6f84ec79eb23bb08a7f00d92dc7093970
FluffyKas <maruzs.zso@gmail.com>
2022-03-02 15:35:45 +0100

    Markup and styling for Todo item
    Adding buttons for deleting items/marking them completed. Adding functionality for delete button. Adding styles to component.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,26 @@
-const Todo = ({todo}) => {
-  return ( 
-    <p>{todo.text}</p>
-   );
+import './Todo.scss';
+import { ReactComponent as CheckIcon } from '../images/icon-check.svg';
+import { ReactComponent as DeleteIcon } from '../images/icon-cross.svg';
+
+
+const Todo = ({ todo, todos, setTodos }) => {
+  const deleteItem = () => {
+    setTodos(todos.filter((element) => element.id !== todo.id));
+  }
+
+  return (
+    <li className="list-item">
+      <span className="item-left">
+        <button className="complete-btn" aria-label="Mark item as completed.">
+          <CheckIcon />
+        </button>
+        {todo.text}
+      </span>
+      <button className="delete-btn" onClick={deleteItem} aria-label="Delete item.">
+        <DeleteIcon />
+      </button>
+    </li>
+  );
 }
- 
+
 export default Todo;
