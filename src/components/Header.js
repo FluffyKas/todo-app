7654a131b11de596debe9e5c58c11b13ed4ba8ee
FluffyKas <maruzs.zso@gmail.com>
2022-03-03 13:32:46 +0100

    Removing unnecessary code

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,15 @@ import { ReactComponent as LightModeIcon } from '../images/icon-sun.svg';
 import { ReactComponent as DarkModeIcon } from '../images/icon-moon.svg';
 
 const Header = ({ switchTheme, theme }) => {
-  console.log(theme);
-    return (
-      <header>
-        <h1>TODO</h1>
-        <button onClick={switchTheme} className="theme-toggle">
-          {theme === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
-        </button>
-      </header>
-    );
-  }
+
+  return (
+    <header>
+      <h1>TODO</h1>
+      <button onClick={switchTheme} className="theme-toggle">
+        {theme === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
+      </button>
+    </header>
+  );
+}
 
 export default Header;
