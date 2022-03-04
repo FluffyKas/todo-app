8471764ce6ba5efafcdafabea53a5c64a576568c
FluffyKas <maruzs.zso@gmail.com>
2022-03-04 13:35:36 +0100

    Adding delete functionality and logic for tracking completion status.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,15 @@ import './TodoList.scss';
 
 import Todo from "./Todo";
 
-const TodoList = ({ todos, setTodos }) => {
-  const deleteList = () => {
-    setTodos('');
+const TodoList = ({ todos, setTodos, setStatus, filteredTodos }) => {
+
+  const deleteCompleted = () => {
+    setTodos(todos.filter(todo => todo.completed === false));
   };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.innerHTML.toLowerCase());
+  }
 
   return (
     <div className="todo-wrapper">
@@ -13,39 +18,42 @@ const TodoList = ({ todos, setTodos }) => {
       {/* TODO LIST */}
       <div className="todo-container">
         <ul className="todo-list">
-          {todos !== '' && todos.map((todo) => {
+          {filteredTodos !== '' && filteredTodos.map((todo) => {
             return <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
           })
           }
         </ul>
+
+        {/* LIST FUNCTIONS */}
         <div className="list-functions">
-          <p className="list-item-count">{todos.length} items left</p>
+          <p className="list-item-count">{filteredTodos.length} items left</p>
           <ul className="list-filters">
-          <li>
-          <button className="filter-btn filter-all">All</button>
-          </li>
-        <li>
-          <button className="filter-btn filter-active">Active</button>
-          </li>
-        <li>
-          <button className="filter-btn filter-completed">Completed</button>
-          </li>
+            <li>
+              <button onClick={statusHandler} className="filter-btn filter-all">All</button>
+            </li>
+            <li>
+              <button onClick={statusHandler} className="filter-btn filter-active">Active</button>
+            </li>
+            <li>
+              <button onClick={statusHandler} className="filter-btn filter-completed">Completed</button>
+            </li>
           </ul>
-          <button className="clear-list-btn" onClick={deleteList}>Clear completed</button>
+          <button className="clear-list-btn" onClick={deleteCompleted}>Clear completed</button>
         </div>
+
       </div>
 
       {/* FILTER MENU FOR MOBILE */}
       <ul className="list-filters-mobile">
         <li>
-          <button className="filter-btn filter-all">All</button>
-          </li>
+          <button onClick={statusHandler} className="filter-btn filter-all">All</button>
+        </li>
         <li>
-          <button className="filter-btn filter-active">Active</button>
-          </li>
+          <button onClick={statusHandler} className="filter-btn filter-active">Active</button>
+        </li>
         <li>
-          <button className="filter-btn filter-completed">Completed</button>
-          </li>
+          <button onClick={statusHandler} className="filter-btn filter-completed">Completed</button>
+        </li>
       </ul>
     </div>
   );
