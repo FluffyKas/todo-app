942c8f8df99bf96dfdc1ab543790d5f40f87beae
FluffyKas <maruzs.zso@gmail.com>
2022-03-04 13:32:19 +0100

    Adding logic for filtering and saving to local storage.

--- src/components/TodoController.js
+++ src/components/TodoController.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CreateTodo from './CreateTodo';
 import TodoList from './TodoList';
 import './TodoController.scss';
@@ -6,11 +6,57 @@ import './TodoController.scss';
 const TodoController = () => {
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+ //Saving to local storage (needs rework with npm package)
+
+ const saveLocalTodos = () => {
+   localStorage.setItem("todos", JSON.stringify(todos))
+ };
+
+ const getLocalTodos = () => {
+   if(localStorage.getItem("todos") === null) {
+     localStorage.setItem("todos", JSON.stringify([]));
+   } else {
+     let todoLocal = JSON.parse(localStorage.getItem("todos"))
+     setTodos(todoLocal);
+   }
+ };
+
+  const todoFilterer = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'active': 
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+    }
+  }
+
+  useEffect(() => {
+    getLocalTodos();
+  }, [])
+
+  useEffect(() => {
+    todoFilterer();
+    saveLocalTodos();
+  }, [todos, status]);
 
   return ( 
     <main>
       <CreateTodo todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList 
+      todos={todos}
+      setTodos={setTodos} 
+      status={status} 
+      setStatus={setStatus} 
+      filteredTodos={filteredTodos} 
+      setFilteredTodos={setFilteredTodos}
+      />
       <p className="drag-and-drop-msg">Drag and drop to reorder list</p>
     </main>
    );
