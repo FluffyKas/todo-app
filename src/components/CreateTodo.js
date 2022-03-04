ef705c879e67dc69cc28bba6bfbe8857678ba2d2
FluffyKas <maruzs.zso@gmail.com>
2022-03-04 13:36:34 +0100

    adding decoration button to match todo list items.

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -15,7 +15,10 @@ const CreateTodo = ({ inputText, setInputText, todos, setTodos }) => {
   }
 
   return (
-    <form>
+    <form className="create-todo-form">
+      <button className="decor-btn" aria-label="Mark item as completed.">
+        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="none" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+      </button>
       <input type="text" onChange={handleChange} onKeyPress={submitHandler} value={inputText} placeholder="Create a new todo..." />
     </form>
   );
